1e1339555d00353bdaa4fc22d711aea021ec15af
verma-sagar <sagarvermat20@gmail.com>
2020-05-31 21:36:51 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="shortcut icon" href="images/icon.png">
+<title>Myra Saffron</title>
+<meta name="description" content="CHAPANERKAR SAFFRON IMPEX PVT LTD - Supplier,Wholesaler of Pure Saffron from Shrirampur Maharashtra, India.">
+<meta name="keywords" content="Saffron Supplier,Wholesaler In Shrirampur Maharashtra">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" /> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style.css"></link>
+</head>
+<body>
+<!-- Header -->
+<>
+<!-- Nav Bar -->
+<nav class="navbar navbar-expand-sm navbar-absolute navbar-dark">
+<!-- Logo -->
+<a class="navbar-brand" href="#">
+<img src="images/logo.png" class="float-left" alt="Logo" width=200 height=100/>
+</a>
+<!-- Collaspe -->
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon fas-fa-bars"></span>
+  </button>
+
+  <!-- Links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item mx-5">
+      <a class="nav-link" href="#about">About</a>
+    </li>
+    <li class="nav-item mx-5">
+      <a class="nav-link" href="products">Products</a>
+    </li>
+    <li class="nav-item mx-5">
+      <a class="nav-link" href="contact">Contact Us</a>
+    </li>
+  </ul>
+</div>
+</nav>
+<!-- Nav Bar -->
+
+<div class="container">
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slider3.jpeg" class="img-fluid" alt="slider1" width="1100" height="450">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider3.jpeg" class="img-fluid" alt="sider2" width="1100" height="450">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider3.jpeg" class="img-fluid" alt="sidler3" width="1100" height="450">
+    </div>
+  </div>
+</div>
+</div>
+<!-- Header -->
+<!-- About -->
+<section id="about">
+<div class="container">
+
+</div>
+
+</section>
+<!-- About -->
+<!-- Products -->
+ <section id="products" class="p-5">
+        <div class="container">
+            <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="images/slider3.jpeg" alt="">
+                </div>
+                <div class="col-lg-6">
+                    <div class="bg-black text-center h-100">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-white">Product 1</h4>
+                                <p class="mb-0 text-white">An example of where you can put an image of a product, or anything else, along with a description.</p>
+                                <hr class="d-none d-lg-block mb-0 ml-0">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center no-gutters">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="images/slider3.jpeg" alt="">
+                </div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="bg-black text-center h-100">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                                <h4 class="text-white">Product 2</h4>
+                                <p class="mb-0 text-white">Another example of a product with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                <hr class="d-none d-lg-block mb-0 mr-0">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="images/slider3.jpeg" alt="">
+                </div>
+                <div class="col-lg-6">
+                    <div class="bg-black text-center h-100">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-white">Product 3</h4>
+                                <p class="mb-0 text-white">An example of where you can put an image of a product, or anything else, along with a description.</p>
+                                <hr class="d-none d-lg-block mb-0 ml-0">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center no-gutters">
+                <div class="col-lg-6">
+                    <img class="img-fluid" src="images/slider3.jpeg" alt="">
+                </div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="bg-black text-center h-100">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                                <h4 class="text-white">Product 4</h4>
+                                <p class="mb-0 text-white">Another example of a product with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                <hr class="d-none d-lg-block mb-0 mr-0">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</section>
+<!-- Products -->
+<!-- Footer -->
+<footer id="footer">
+  <div class="container text-center">
+  <address>
+  © 2020 Copyright: Created by Sagar Verma
+  <div class="d-flex flex-column">
+<a href="mailto:sagarvermat20@gmail.com" class="p-2">Mail Me</a>
+<a href="tel:8130716711">Call Me</a>
+</address>
+  </div>
+</div>
+</footer>
+<!-- Footer -->
+</body>
+</html>
