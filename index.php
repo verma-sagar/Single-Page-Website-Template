83615adfc7351d2a02d962d53e8e007b32937baf
Saga Verma <44877266+verma-sagar@users.noreply.github.com>
2020-06-06 23:05:02 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -11,12 +11,13 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/c18e4f8815.js"></script>
 <link rel="stylesheet" href="style.css"></link>
 </head>
 <body>
 <!-- Header -->
 <!-- Nav Bar -->
-<nav class="navbar navbar-expand-sm navbar-absolute navbar-dark">
+<nav class="navbar navbar-expand-sm navbar-absolute navbar-dark" style="padding:0px;">
 <!-- Logo -->
 <a class="navbar-brand" href="#">
 <img src="images/logo.png" class="float-left" alt="Logo" width=200 height=100/>
@@ -43,22 +44,29 @@
 </nav>
 <!-- Nav Bar -->
 
-<div class="container">
 <div id="demo" class="carousel slide" data-ride="carousel">
+  
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="images/slider3.jpeg" class="img-fluid" alt="slider1" width="1100" height="450">
+      <img src="images/slider3.jpeg" class="img-fluid d-block mx-auto" alt="slider1" width="950" height="400">
     </div>
     <div class="carousel-item">
-      <img src="images/slider3.jpeg" class="img-fluid" alt="sider2" width="1100" height="450">
+      <img src="images/slider3.jpeg" class="img-fluid d-block mx-auto" alt="sider2" width="950" height="400">
     </div>
     <div class="carousel-item">
-      <img src="images/slider3.jpeg" class="img-fluid" alt="sidler3" width="1100" height="450">
+      <img src="images/slider3.jpeg" class="img-fluid d-block mx-auto" alt="sidler3" width="950" height="400">
     </div>
   </div>
+   <!-- Left and right controls -->
+   <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon d-none d-xl-block"></span>
+  </a>
 </div>
-</div>
+
 <!-- Header -->
 <!-- About -->
 <section id="about">
@@ -71,14 +79,14 @@
 <!-- Products -->
  <section id="products" class="p-5">
         <div class="container">
-            <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="row justify-content-center no-gutters mb-4 mb-lg-0">
                 <div class="col-lg-6">
                     <img class="img-fluid" src="images/slider3.jpeg" alt="">
                 </div>
                 <div class="col-lg-6">
                     <div class="bg-black text-center h-100">
                         <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                            <div class="project-text w-100 my-auto text-center text-lg-left p-2">
                                 <h4 class="text-white">Product 1</h4>
                                 <p class="mb-0 text-white">An example of where you can put an image of a product, or anything else, along with a description.</p>
                                 <hr class="d-none d-lg-block mb-0 ml-0">
@@ -88,14 +96,14 @@
                 </div>
             </div>
 
-            <div class="row justify-content-center no-gutters">
+            <div class="row justify-content-center no-gutters mb-4 mb-lg-0">
                 <div class="col-lg-6">
                     <img class="img-fluid" src="images/slider3.jpeg" alt="">
                 </div>
                 <div class="col-lg-6 order-lg-first">
                     <div class="bg-black text-center h-100">
                         <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                            <div class="project-text w-100 my-auto text-center text-lg-right p-2">
                                 <h4 class="text-white">Product 2</h4>
                                 <p class="mb-0 text-white">Another example of a product with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
                                 <hr class="d-none d-lg-block mb-0 mr-0">
@@ -104,14 +112,14 @@
                     </div>
                 </div>
             </div>
-            <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+            <div class="row justify-content-center no-gutters mb-4 mb-lg-0">
                 <div class="col-lg-6">
                     <img class="img-fluid" src="images/slider3.jpeg" alt="">
                 </div>
                 <div class="col-lg-6">
                     <div class="bg-black text-center h-100">
                         <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                            <div class="project-text w-100 my-auto text-center text-lg-left p-2">
                                 <h4 class="text-white">Product 3</h4>
                                 <p class="mb-0 text-white">An example of where you can put an image of a product, or anything else, along with a description.</p>
                                 <hr class="d-none d-lg-block mb-0 ml-0">
@@ -128,7 +136,7 @@
                 <div class="col-lg-6 order-lg-first">
                     <div class="bg-black text-center h-100">
                         <div class="d-flex h-100">
-                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                            <div class="project-text w-100 my-auto text-center text-lg-right p-2">
                                 <h4 class="text-white">Product 4</h4>
                                 <p class="mb-0 text-white">Another example of a product with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
                                 <hr class="d-none d-lg-block mb-0 mr-0">
@@ -142,16 +150,96 @@
 </section>
 <!-- Products -->
 <!-- Footer -->
-<footer id="footer">
-  <div class="container text-center">
-  <address>
-  © 2020 Copyright: Created by Sagar Verma
-  <div class="d-flex flex-column">
-<a href="mailto:sagarvermat20@gmail.com" class="p-2">Mail Me</a>
-<a href="tel:8130716711">Call Me</a>
-</address>
+<footer>
+
+  <!-- Footer Links -->
+  <div class="text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mx-auto">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-4 mb-4 text-center">
+
+        <!-- Content -->
+        <h6 class="font-weight-bold text-center text-white">Myra Saffron-A World of Healthy Taste</h6>
+        <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p class="text-white text-center">MYRA SAFFRON provides best quality and is one of trusted brand in india when it comes to selling poushal, sargol, negin, super negin saffron directly purchased from Iran, to serve end user, retailers and whole sellers worldwide.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-4 mb-4 text-center">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold text-white">Products</h6>
+        <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a class="text-white" href="#!">Poushal</a>
+        </p>
+        <p>
+          <a class="text-white" href="#!">Sargol</a>
+        </p>
+        <p>
+          <a class="text-white" href="#!">Negin</a>
+        </p>
+        <p>
+          <a class="text-white" href="#!">Super Negin</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-4 mb-4 text-center">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold text-white">Our Quality Policy</h6>
+        <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p class="text-white">Pure</p>
+        <p class="text-white">100% Natural</p>
+        <p class="text-white">Fairly Traded</p>
+        <p class="text-white">20% Discount On Orders Above 100gms</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-4 mb-md-0 mb-4 text-white text-center">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-2"></i>Shrirampur,Ahmednagar-413709, Maharashtra, India</p>
+        <p>
+          <i class="fas fa-envelope mr-2"></i> info@example.com</p>
+        <p>
+          <i class="fas fa-phone mr-2"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-phone mr-2"></i> + 01 234 567 89</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
   </div>
+  <!-- Footer Links -->
+  <!-- Copyright -->
+  <div class="container text-center text-white">
+  <address>
+  © 2020 Copyright Created by Sagar Verma
+  <div>
+<a href="mailto:sagarvermat20@gmail.com" class="text-white">Click To Mail Me</a>&nbspor
+<a href="tel:8130716711" class="text-white">Click To Call Me</a>
+</address>
 </div>
+</div>
+  <!-- Copyright -->
+
 </footer>
 <!-- Footer -->
 </body>
